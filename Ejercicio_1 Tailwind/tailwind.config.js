c11c1adabbc656908e46448d8713320347bbaa2f
Adrián Sorroche <asorrocheL@protonmail.com>
2023-10-11 15:42:19 +0200

    feat: Ejercicio_0 Tailwind y Ejercicio_1 Tailwind

---
+++ Ejercicio_1 Tailwind/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        colors: {
+          marron: '#b58989',
+          gris_oscuro: '#403f3f'
+        },
+    },
+    plugins: [],
+  }
+}
