7f49f4dc60f0931506e5089d0e580b89a95d580a
adrian <asorroche@protonmail.com>
2023-10-18 17:45:01 +0200

    feat: Add Ejercicio Curriculum en proceso

---
+++ Ejercicio_curriculum/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'gray/dark': '#47516B',
+        'gray/darker': '#2E2E48',
+        'gray/default': '#79819A',
+        'gray/lightest': '#E2E6EE',
+        'socilal/linkedin': '#0077B5',
+        
+    },
+  },
+  plugins: [],
+  },
+};
