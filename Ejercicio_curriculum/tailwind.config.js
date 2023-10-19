0a601958cec4a7b0c0c46aece259e40752194cf0
Adrián Sorroche <asorrocheL@protonmail.com>
2023-10-19 13:34:00 +0200

    feat: Add Version Desktop

--- Ejercicio_curriculum/tailwind.config.js
+++ Ejercicio_curriculum/tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
         'gray/default': '#79819A',
         'gray/lightest': '#E2E6EE',
         'socilal/linkedin': '#0077B5',
+        'secondary/default': '#516CF7',
+        'primary/lighter': '#EFE2F9',
+        'purple': '#9251F7',
         
     },
   },
