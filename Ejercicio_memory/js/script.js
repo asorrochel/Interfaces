1aaa33c3ff8c881d1e525802ca6192694d4340fb
adrian <asorroche@protonmail.com>
2023-11-06 17:26:34 +0100

    Refactored code to improve performance and readability.

---
+++ Ejercicio_memory/js/script.js
@@ -0,0 +1,36 @@
+let carta = null;
+const contador_aciertos = document.getElementById('contador_aciertos');
+const contador_fallos = document.getElementById('contador_fallos');
+
+function compararCarta(cartaNueva) {
+    if (carta == null) {
+        carta = cartaNueva;
+        return;
+    }
+
+    if (cartaNueva.attributes['name'].value == carta.attributes['name'].value) {
+        carta.classList.add('correcta');
+        cartaNueva.classList.add('correcta');
+        contador_aciertos.innerHTML++;
+    } else {
+        carta.classList.remove('girada');
+        cartaNueva.classList.remove('girada');
+        contador_fallos.innerHTML++;
+
+    }
+    carta = null;
+}
+
+function girarCarta(cartaNueva) {
+    if (!cartaNueva.classList.contains('girada')) {
+        cartaNueva.classList.add('girada');
+        setTimeout(compararCarta, 500, cartaNueva);
+    }
+}
+
+document.getElementById('resetButton').addEventListener('click', () => {
+    const cartas = document.querySelectorAll('.carta');
+    cartas.forEach((carta) => {
+        carta.classList.remove('girada', 'correcta');
+    });
+});
